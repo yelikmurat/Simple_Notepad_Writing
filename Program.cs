a43c97f1ea0fa48ee66932bcd9eb5276138b9679
yelikmurat <116756621+yelikmurat@users.noreply.github.com>
2024-01-26 20:59:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using WindowsInput;
+
+class Program
+{
+    static void Main()
+    {
+        // Start Notepad
+        Process notepadProcess = Process.Start("notepad.exe");
+
+        Thread.Sleep(2000);
+
+        SendKeysToNotepad("Hi, this is Eli and this is my test project");
+
+        Thread.Sleep(2000);
+
+        // Close Notepad
+        notepadProcess.CloseMainWindow();
+    }
+
+    static void SendKeysToNotepad(string message)
+    {
+        var simulator = new InputSimulator();
+        simulator.Keyboard.TextEntry(message);
+    }
+}
